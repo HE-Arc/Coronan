27efa40a16ac927e0509d1a820c3cc1f1170f7dc
Adrian <adrian.buzala@gmail.com>
2021-03-19 09:47:00 +0100

    basic pages created for proĵect structure

--- coronanapp/urls.py
+++ coronanapp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from coronanapp import views
 
 urlpatterns = [
-    path('', views.hello_world, name='hello_world'),
+    path('', views.home, name='home'),
+    path('profile', views.profile, name='profile'),
+    path('addFunds', views.add_funds, name='add_funds'),
+    path('signUp', views.sign_up, name='sign_up'),
+    path('login', views.login, name='login'),
+    path('addBet', views.add_bet, name='add_bet'),
+    path('bets', views.bets, name='bets'),
 ]
--- coronanapp/views.py
+++ coronanapp/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
-def hello_world(request):
-    return render(request, 'hello_world.html', {})
+def home(request):
+    return render(request, 'home.html', {})
+
+def profile(request):
+    return render(request, 'profile.html', {})
+
+def add_funds(request):
+    return render(request, 'add_funds.html', {})
+
+def sign_up(request):
+    return render(request, 'sign_up.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
+
+def add_bet(request):
+    return render(request, 'add_bet.html', {})
+
+def bets(request):
+    return render(request, 'bets.html', {})
