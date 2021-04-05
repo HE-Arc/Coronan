d27cc27a4b7d49621debd092028001074ae506ca
Adrian <adrian.buzala@gmail.com>
2021-04-05 15:42:58 +0200

    bets migration made

--- coronanapp/models.py
+++ coronanapp/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+class Bet(models.Model):
+    moneyBet = models.IntegerField(blank=True, null=True)
+    moneyWon = models.IntegerField(blank=True, null=True)
+    ratio = models.IntegerField(blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    cases = models.IntegerField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     funds = models.IntegerField(blank=True, null=True)
